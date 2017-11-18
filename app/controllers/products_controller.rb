ec88f2a39dfb148b29ffe3494abcb03e8a9ff7a1
noelle <saltanoelle@gmail.com>
2017-11-17 18:49:53 -0800

    changed admin

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   
-  before_action :authenticate_admin!, except:[:index, :show, :search]
+  # before_action :authenticate_admin!, except:[:index, :show, :search]
 
   def index
     
