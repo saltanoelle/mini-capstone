992eb5026d4cd176fb0eef4357166b71c02c2813
noelle <saltanoelle@gmail.com>
2017-11-17 20:00:03 -0800

    rake seed for suppliers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 Product.destroy_all
 Supplier.destroy_all
 Image.destroy_all
-Product.create!([
-  {name: "Tory Burch", price: "120.00", description: "Metallic Gold Flats", supplier_id:1},
-  {name: "Adidas NMD", price: "50.00", description: "Pastel NMD shoes",supplier_id:2 },
-  {name: "Stan Smith", price: "60.00", description: "White Casual Shoes", supplier_id:3}
-])
+# Product.create!([
+#   {name: "Tory Burch", price: "120.00", description: "Metallic Gold Flats", supplier_id:1},
+#   {name: "Adidas NMD", price: "50.00", description: "Pastel NMD shoes",supplier_id:2 },
+#   {name: "Stan Smith", price: "60.00", description: "White Casual Shoes", supplier_id:3}
+# ])
 
 Supplier.create!([
   {
@@ -25,24 +25,24 @@ Supplier.create!([
   }
   ])
 
-Image.create!([
-  {
-  url:"http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=55513360", 
-  product_id: 1
-  },
-  {
-  url:"https://s-media-cache-ak0.pinimg.com/236x/1f/15/a0/1f15a0f607ffbf6de434941b24f09dc2.jpg", 
-  product_id: 2
-  },
-  {
-  url:"http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=157100393", 
-  product_id: 3
-  },
-  {
-  url:"http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=123169891", 
-  product_id: 1
-  }
-  ])
+# Image.create!([
+#   {
+#   url:"http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=55513360", 
+#   product_id: 1
+#   },
+#   {
+#   url:"https://s-media-cache-ak0.pinimg.com/236x/1f/15/a0/1f15a0f607ffbf6de434941b24f09dc2.jpg", 
+#   product_id: 2
+#   },
+#   {
+#   url:"http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=157100393", 
+#   product_id: 3
+#   },
+#   {
+#   url:"http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=123169891", 
+#   product_id: 1
+#   }
+#   ])
 
 # image: "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=55513360"
 # image: "https://s-media-cache-ak0.pinimg.com/236x/1f/15/a0/1f15a0f607ffbf6de434941b24f09dc2.jpg"
